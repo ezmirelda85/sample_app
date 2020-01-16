05ca957ed92be0bfe9c66713e5dfb198e468ae85
Ubuntu <ubuntu@ip-172-31-25-185.us-east-2.compute.internal>
2020-01-16 04:43:23 +0000

    Finish layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,5 +9,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     get contact_path
     assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
