56dece526b19f70e57e329dae17dfd3c383bfe9b
Ubuntu <ubuntu@ip-172-31-25-185.us-east-2.compute.internal>
2020-01-23 22:59:13 +0000

    Finish user dignup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,7 +11,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'alert alert-danger'
-    assert_select 'error_explanation'
+    assert_select '.alert.alert-danger'
+    assert_select 'div#error_explanation'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
