ab04f3e5f10ba1c18c44cf2cb6bf04b24f6ba75b
Ubuntu <ubuntu@ip-172-31-25-185.us-east-2.compute.internal>
2020-01-23 18:33:33 +0000

    add error messages and update tests

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'alert alert-danger'
+    assert_select 'error_explanation'
+  end
+end
