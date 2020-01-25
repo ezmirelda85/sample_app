b9efe66edcd10b85efe1d194c7585c71240450fd
Ubuntu <ubuntu@ip-172-31-25-185.us-east-2.compute.internal>
2020-01-25 00:45:46 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,13 @@ class UsersController < ApplicationController
   end
   
   def new
-  @user = User.new
+    @user = User.new
   end
   
   def create
       @user = User.new(user_params)
       if @user.save
+        log_in @user
         flash[:success] ="Welcome to the Sample App!"
         redirect_to @user
       else
