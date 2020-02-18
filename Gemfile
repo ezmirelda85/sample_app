ad8bc45369ffaa568bc6d10e5ea5142115ea0f8f
Ubuntu <ubuntu@ip-172-31-25-185.us-east-2.compute.internal>
2020-02-18 04:04:41 +0000

    Finih user edit, update, index and destroy actions

--- Gemfile
+++ Gemfile
@@ -4,7 +4,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 gem 'rails',      '6.0.2.1'
 gem 'bcrypt',     '3.1.13', require: true
 gem 'bootstrap-sass', '3.4.1'
+gem 'faker',      '2.1.2'
+gem 'will_paginate', '3.1.8'
 gem 'puma',       '3.12.2'
+gem 'bootstrap-will_paginate', '1.0.0'
 gem 'sass-rails', '5.1.0'
 gem 'webpacker',  '4.0.7'
 gem 'turbolinks', '5.2.0'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
-  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   def show
     @user = User.find(params[:id])
   end
@@ -23,10 +26,7 @@ class UsersController < ApplicationController
   end
   
   def edit
-  end
-  
-  def user_params
-    params.require(:user).permit(:name, :email, :ppassword, :password_confimration)
+    @user = User.find(params[:id])
   end
   
   def update
@@ -38,6 +38,11 @@ class UsersController < ApplicationController
     render 'edit'
     end
   end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end 
   
   private
   
@@ -60,6 +65,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+    # Confirms an admin user
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
 
