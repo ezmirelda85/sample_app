b77e1fa007d06206022d4c88c34596e79032b83c
Ubuntu <ubuntu@ip-172-31-25-185.us-east-2.compute.internal>
2020-02-20 23:13:11 +0000

    change back prod sendgrid but remove single quot

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,10 +56,13 @@ Rails.application.configure do
  
   config.action_mailer.raise_delivery_errors = true
   
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-     :address => 'smtp.sendgrid.net',
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://guarded-lake-81573.herokuapp.com/'
+  
+  config.action_mailer.default_url_options = {host: host}
+   ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
@@ -67,19 +70,6 @@ Rails.application.configure do
     :domain => 'heroku.com',
     :enable_starttls_auto => true
     }
-    
-#  host = 'https://guarded-lake-81573.herokuapp.com/'
-  
-#  config.action_mailer.default_url_options = {host: host}
-#   ActionMailer::Base.smtp_settings = {
-#    :address => 'smtp.sendgrid.net',
-#    :port => '587',
-#    :authentication => :plain,
-#    :user_name => ENV['SENDGRID_USERNAME'],
-#    :password => ENV['SENDGRID_PASSWORD'],
-#    :domain => 'heroku.com',
-#    :enable_starttls_auto => true
-#    }
 
 
   
