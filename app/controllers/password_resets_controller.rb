fa14f87cd399678c05239a8a46ab10252f7f0893
Ubuntu <ubuntu@ip-172-31-25-185.us-east-2.compute.internal>
2020-02-22 20:25:42 +0000

    password controller update

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -40,6 +40,7 @@ class PasswordResetsController < ApplicationController
     def user_params
       params.require(:user).permit(:password, :password_confirmation)
     end  
+    
     # Before filters
     def get_user
       @user = User.find_by(email: params[:email])
