a52b56e942d0ddf5e73e219fa9ffda9f66303467
Ubuntu <ubuntu@ip-172-31-51-242.ec2.internal>
2020-03-06 23:41:15 +0000

    14.2.5

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -11,8 +11,8 @@ class RelationshipsController < ApplicationController
   end
   
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
