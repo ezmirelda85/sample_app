7605af254351e6f216c0c7d76b83afa632e8c7ea
Ubuntu <ubuntu@ip-172-31-51-242.ec2.internal>
2020-03-07 00:06:39 +0000

    finished 14.3.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -86,6 +86,21 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
   
+  #Returns true if a password reset has expired
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+  
+  #Returns a user's status feed
+  def feed
+    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+  end
+  
+  #Follows a user
+  def follow(other_user)
+    following << other_user
+  end
+  
 private
   # Convert email to all lower-case
   def downcase_email 
